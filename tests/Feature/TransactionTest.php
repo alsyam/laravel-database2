1df4d78404d94d95cb74a39780921807dbe3c9bb
alsyam <malsyam69@gmail.com>
2024-06-19 22:26:52 +0700

    first commit

---
+++ tests/Feature/TransactionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Database\QueryException;
+use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TransactionTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::delete('delete from categories');
+    }
+
+    public function testTransactionSuccess()
+    {
+        DB::transaction(function () {
+
+            DB::insert('INSERT INTO categories(id, name, description, created_at) values (?,?,?,?)', [
+                "GADGET", "Gadget", "Gadget Category", "2020-10-10 10:10:10"
+            ]);
+
+            DB::insert('INSERT INTO categories(id, name, description, created_at) values (?,?,?,?)', [
+                "FOOD", "Food", "Food Category", "2020-10-10 10:10:10"
+            ]);
+        });
+
+        $result = DB::select("select * FROM categories");
+        self::assertCount(2, $result);
+    }
+    public function testTransactionFailed()
+    {
+
+        try {
+
+            DB::transaction(function () {
+
+                DB::insert('INSERT INTO categories(id, name, description, created_at) values (?,?,?,?)', [
+                    "GADGET", "Gadget", "Gadget Category", "2020-10-10 10:10:10"
+                ]);
+
+                DB::insert('INSERT INTO categories(id, name, description, created_at) values (?,?,?,?)', [
+                    "GADGET", "Food", "Food Category", "2020-10-10 10:10:10"
+                ]);
+            });
+        } catch (QueryException $error) {
+            // expected
+        }
+
+        $results = DB::select("select * FROM categories");
+        self::assertCount(0, $results);
+    }
+}
