9a8cd5c248453cd18e99b28f073e09b08da17085
alsyam <malsyam69@gmail.com>
2024-06-24 23:40:35 +0700

    lanjutkan

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -187,4 +187,20 @@ class QueryBuilderTest extends TestCase
             Log::info(json_encode($item));
         });
     }
+
+    public function testIncrement()
+    {
+
+
+        DB::table("counters")->where('id', '=', 'sample')->increment('counter', 1);
+
+
+        $collection = DB::table("counters")
+            ->where("id", "=", "sample")
+            ->get();
+        self::assertCount(1, $collection);
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+    }
 }
